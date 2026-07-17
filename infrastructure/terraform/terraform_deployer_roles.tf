46ad3e6aa3288e17c70db385a572fde775fdb1e3
agenticaipoc777 <lakshmikanth.avh1bgmail.com>
2026-07-17 14:03:03 +0200

    fix

--- infrastructure/terraform/terraform_deployer_roles.tf
+++
@@ -1,8 +0,0 @@
-# ====================================================================
-# ELEVATE TF-DEPLOYER TO MANAGE PROJECT POLICIES
-# ====================================================================
-resource "google_project_iam_member" "tf_deployer_iam_admin" {
-  project = var.project_id
-  role    = "roles/resourcemanager.projectIamAdmin"
-  member  = "serviceAccount:tf-deployer@${var.project_id}.iam.gserviceaccount.com"
-}
